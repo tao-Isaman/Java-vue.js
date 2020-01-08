6cc73f646a88301186bb857401e34e8f51093b2b
ManH <b59153301234@gmail.com>
2020-01-08 20:28:39 +0700

    Add UI - close #21

--- backend/src/main/java/com/cpe/backend/Bed/repository/NurseRepository.java
+++ backend/src/main/java/com/cpe/backend/Bed/repository/NurseRepository.java
@@ -1,10 +1,8 @@
 package com.cpe.backend.Bed.repository;
 
 import com.cpe.backend.Bed.entity.Nurse;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
 @RepositoryRestResource
 public
 interface NurseRepository extends JpaRepository<Nurse, Long> {
