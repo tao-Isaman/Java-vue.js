1f4fbb0848bb45a33e8239454ad33b0ff8ed76cc
ManH <b59153301234@gmail.com>
2020-01-27 21:37:14 +0700

    แก้ไขการบันทึกเวลาของระบบจองเตียง

--- backend/src/main/java/com/cpe/backend/Bed/repository/ReservationRepository.java
+++ backend/src/main/java/com/cpe/backend/Bed/repository/ReservationRepository.java
@@ -13,9 +13,7 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 public
 interface ReservationRepository extends JpaRepository<Reservation, Long> {
     Reservation findById(long id);
-    
     @Query(value ="SELECT b.BED_ID,b.Price,b.TYPE,r.RESERV_DATE,r.PATIENT_ID,r.RESERVATION_ID,p.name FROM BED as b  INNER JOIN RESERVATION as r inner join PATIENT as p on b.BED_ID = r.BEDS_ID and p.NATIONAL_ID = r.PATIENT_ID  where PATIENT_ID =:nationalID",
      nativeQuery = true)
      Collection<Object[]> findByPatientID(@Param("nationalID") long nationalID);
-     
 }
